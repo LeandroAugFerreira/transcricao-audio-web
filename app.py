7a74860a03a42b28baa890c8bc578e83aec50570
Leandro Ferreira <leandroguto1@gmail.com>
2026-09-10 22:34:22 -0300

    Cria interface inicial com Flask e CSS

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
-print("Projeto de Transcrição iniciado!")
+# Importa o Flask e a função responsável por carregar arquivos HTML
+from flask import Flask, render_template
+
+
+# Cria a nossa aplicação Flask
+app = Flask(__name__)
+
+
+# Define a página inicial do sistema
+@app.route("/")
+def inicio():
+
+    # Procura o arquivo index.html dentro da pasta templates
+    return render_template("index.html")
+
+
+# Inicia o servidor quando executamos este arquivo
+if __name__ == "__main__":
+    app.run(debug=True)
